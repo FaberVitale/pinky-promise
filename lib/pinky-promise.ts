b1d58ef4ea45a087f37b3d4f341e38fceb68b94a
FaberVitale <faber.vitale@gmail.com>
2023-11-07 19:51:55 +0100

    feat: implement promise constructor

--- lib/pinky-promise.ts
+++ lib/pinky-promise.ts
@@ -10,10 +10,13 @@
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#description
  */
 type PromiseState<T> = Readonly<
-  | { status: "pending" }
+  | { status: "pending"; thenable?: PromiseLike<T> }
   | { status: "fulfilled"; value: T }
   | { status: "rejected"; reason: any }
 >;
+
+type OnFulfill<T> = NonNullable<Parameters<PromiseLike<T>["then"]>["0"]>;
+type OnReject = NonNullable<Parameters<PromiseLike<unknown>["then"]>["1"]>;
 
 /**
  * The callback passed as argument of PinkyPromise constructor.
@@ -25,8 +28,18 @@ export type PinkyPromiseExecutor<T> = (
   reject: (reason?: any) => void,
 ) => void;
 
+function isThenable<T>(value: unknown): value is PromiseLike<T> {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    typeof (value as Record<string, unknown>).then === "function"
+  );
+}
+
 export class PinkyPromise<T> {
   #state: PromiseState<T> = { status: "pending" };
+  #fulfilledListeners: OnFulfill<T>[] = [];
+  #rejectedListeners: OnReject[] = [];
 
   constructor(executor: PinkyPromiseExecutor<T>) {
     if (typeof executor !== "function") {
@@ -40,7 +53,35 @@ export class PinkyPromise<T> {
     }
   }
 
-  #resolve = (value: T | PromiseLike<T>) => {};
+  #verifyState() {
+  }
 
-  #reject = (reason: unknown) => {};
+  /**
+   * Callback passed to the Promise executor
+   * @see https://tc39.es/ecma262/multipage/control-abstraction-objects.html#sec-promise-resolve-functions
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/Promise#resolver_function
+   */
+  #resolve = (value: T | PromiseLike<T>): void => {
+    if (this.#state.status === "pending" && !this.#state.thenable) {
+      if (isThenable(value)) {
+        // throw exception to avoid loops
+        if (Object.is(value, this)) {
+          throw new TypeError("Chaining cycle detected for promise");
+        }
+
+        value.then(this.#resolve, this.#reject);
+      } else {
+        this.#state = { status: "fulfilled", value };
+      }
+
+      this.#verifyState();
+    }
+  };
+
+  #reject = (reason: unknown): void => {
+    if (this.#state.status === "pending" && !this.#state.thenable) {
+      this.#state = { status: "rejected", reason };
+      this.#verifyState();
+    }
+  };
 }
